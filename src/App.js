35363bca6eee878192bfdab2211ab54a09b9fc9a
You Song Hou <yousonghou@gmail.com>
2022-05-11 02:40:40 -0400

    refactor: downgrade react app to use webpack 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Container, Button } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Button } from "@chakra-ui/core";
 
 function App() {
   return (
-    <Container maxW='1100px'>
+    <Box p={4}>
       <h1>Recovery Crypto Management</h1>
       <Button colorScheme='blue'>Get Started</Button>
-    </Container>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from "@chakra-ui/core";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ChakraProvider>
+ReactDOM.render(
+  <ThemeProvider>
     <App />
-  </ChakraProvider>
+  </ThemeProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
