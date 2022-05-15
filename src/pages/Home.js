c0cfbf4d1c87ad16ebb0f22c555a8c81ca33747f
You Song Hou <yousonghou@gmail.com>
2022-05-15 00:37:14 -0400

    feat: install zksync-web3 and initialize Provider and Signer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import { Button } from "@chakra-ui/core";
 import Web3 from 'web3';
+import { Contract, Web3Provider, Provider } from "zksync-web3";
+import { ethers } from "ethers";
 
+import RecoverableSafeFactory from '../artifacts-zk/contracts/RecoverableSafeFactory.sol/RecoverableSafeFactory.json';
 import { connectCB } from '../config/coinbase-wallet';
+
+const GREETER_CONTRACT_ADDRESS = "0x3D63e9B9269f937e8C1D372E01AB29Ee48c9d111";
+const GREETER_CONTRACT_ABI = RecoverableSafeFactory.abi;
 
 function Home() {
   const [ethAddress, setEthAddress] = useState("");
+  console.log(GREETER_CONTRACT_ABI)
 
   const connectCoinbaseWallet = () => {
     const ethereum = connectCB();
@@ -22,11 +29,36 @@ function Home() {
 
     console.log(web3);
   }
+
+  const connectMetaMask = async () => {
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(() => {
+        if (+window.ethereum.networkVersion == 280) {
+          const provider = new Provider('https://zksync2-testnet.zksync.dev');
+    
+          // Note that we still need to get the Metamask signer
+          const signer = (new Web3Provider(window.ethereum)).getSigner();
+
+          const contract = new Contract(
+              GREETER_CONTRACT_ADDRESS,
+              GREETER_CONTRACT_ABI,
+              signer
+          );
+
+          console.log(provider, contract);
+        } else {
+          alert("Please switch network to zkSync!");
+        }
+      })
+      .catch((e) => console.log(e)); 
+  }
+
   return (
     <div>
       <h1>Recovery Crypto Management</h1>
       <p>ETH Address: {ethAddress}</p>
       <Button colorScheme='blue' onClick={connectCoinbaseWallet}>Get Started</Button>
+      <Button colorScheme='blue' onClick={connectMetaMask}>Connect Wallet</Button>
     </div>
   )
 }
