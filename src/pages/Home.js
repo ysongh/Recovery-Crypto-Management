db8dd241bfa40e2593acf8a6c7d4f98f72d5b4fd
You Song Hou <yousonghou@gmail.com>
2022-05-18 02:33:32 -0400

    feat: connect the front end to the new smart contract

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,17 @@ import { ethers } from "ethers";
 import RecoverableSafeFactory from '../artifacts-zk/contracts/RecoverableSafeFactory.sol/RecoverableSafeFactory.json';
 import { connectCB } from '../config/coinbase-wallet';
 
-const RSF_CONTRACT_ADDRESS = "0x86498B8C456454305ED75f9C87e5687FC6B95CA9";
+const RSF_CONTRACT_ADDRESS = "0x63b3F646e124F161AC3ff9C7AA4E35a8a3F733A4";
 const RSF_CONTRACT_ABI = RecoverableSafeFactory.abi;
 
 function Home() {
   const [ethAddress, setEthAddress] = useState("");
   const [ethBalance, setETHBalance] = useState("");
   const [safeETHBalance, setSafeETHBalance] = useState(0);
-  const [rsContract, setRSContract] = useState(""); 
+  const [rsContract, setRSContract] = useState("");
+  const [userSigner, setUserSigner] = useState(""); 
   const [depositAmount, setDepositAmount] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
 
   const connectCoinbaseWallet = () => {
     const ethereum = connectCB();
@@ -41,6 +43,7 @@ function Home() {
     
           // Note that we still need to get the Metamask signer
           const signer = (new Web3Provider(window.ethereum)).getSigner();
+          setUserSigner(signer);
 
           const contract = new Contract(
             RSF_CONTRACT_ADDRESS,
@@ -60,8 +63,8 @@ function Home() {
   }
 
   const getBalance = async () => {
-    const balance = await rsContract.getETHBalance("0");
-    console.log(balance);
+    const balance = await rsContract.getTokenAllowance("0", "0x5C221E77624690fff6dd741493D735a17716c26B");
+    console.log(balance.toString());
     setSafeETHBalance(balance.toString());
   }
 
@@ -76,18 +79,27 @@ function Home() {
     await txHandle.wait();
   }
 
-  const depositETH = async () => {
+  const depositToSafe = async () => {
+    const transferHandle = userSigner.transfer({
+      to: "0xf5F2e25c091A8449cc41185937E0217151dcd7Ee",
+      token: "0x5C221E77624690fff6dd741493D735a17716c26B",
+      amount: ethers.utils.parseEther(depositAmount),
+      feeToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    });
+
+    console.log(transferHandle);
+  }
+
+  const withdrawToken = async () => {
     try {
-      const txHandle = await rsContract.depositETHToSafe("0", {
+      const txHandle = await rsContract.withdrawTokenfromSafe("0", "0x5C221E77624690fff6dd741493D735a17716c26B", ethers.utils.parseEther(withdrawAmount), {
         customData: {
           // Passing the token to pay fee with
-          feeToken: "0x5C221E77624690fff6dd741493D735a17716c26B",
-          value: depositAmount
+          feeToken: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
         },
       });
   
       await txHandle.wait();
-      await getBalance();
     }
     catch(error) {
       console.error(error);
@@ -120,8 +132,13 @@ function Home() {
           <br />
           <p>Safe ETH balance: {safeETHBalance}</p>
           <input value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} />
-          <Button variant="contained" onClick={depositETH}>
+          <Button variant="contained" onClick={depositToSafe}>
             Deposit
+          </Button>
+          <br />
+          <input value={withdrawAmount} onChange={(e) => setWithdrawAmount(e.target.value)} />
+          <Button variant="contained" onClick={withdrawToken}>
+            WithDraw
           </Button>
         </>
       )}
