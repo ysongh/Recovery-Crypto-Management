d566b9718ae29562d13924dceeb56dc4aa08a08f
You Song Hou <yousonghou@gmail.com>
2022-10-19 00:30:09 -0400

    refactor: move Sidebar to its own component

---
+++ src/components/safe-dashboard/Sidebar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Drawer, Toolbar, List, Divider, ListItem, ListItemIcon, ListItemText, ListItemButton } from '@mui/material';
+
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DataSaverOnIcon from '@mui/icons-material/DataSaverOn';
+
+const drawerLinks = [
+  {
+    "text": "Your Wallet",
+    "icon": <AccountBalanceWalletIcon />
+  },
+  {
+    "text": "Your Safe",
+    "icon": <InventoryIcon />
+  },
+  {
+    "text": "Setting",
+    "icon": <SettingsIcon />
+  },
+  {
+    "text": "Recover",
+    "icon": <DataSaverOnIcon />
+  }
+];
+const drawerWidth = 200;
+
+function Sidebar({ ethAddress, safeAddress, setCurrentSection }) {
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <Divider />
+      <List>
+        {drawerLinks.map((d, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {d.icon}
+              </ListItemIcon>
+              <ListItemText primary={d.text} onClick={() => setCurrentSection(d.text)}/>
+            </ListItemButton>
+          </ListItem>
+        ))}
+        <Divider />
+        <ListItem>
+          <div>
+            <ListItemText primary="ETH Address" />
+            <a href={`https://zksync2-testnet.zkscan.io/address/${ethAddress}`} target="_blank" rel="noopener noreferrer">
+              {ethAddress && ethAddress.substring(0,8) + "..." + ethAddress.substring(34,42)}
+            </a>
+          </div>
+        </ListItem>
+        <ListItem>
+          <div>
+            <ListItemText primary="Safe Address" />
+            <a href={`https://zksync2-testnet.zkscan.io/address/${safeAddress}`} target="_blank" rel="noopener noreferrer">
+              {safeAddress && safeAddress.substring(0,8) + "..." + safeAddress.substring(34,42)}
+            </a>
+          </div>
+        </ListItem>
+      </List>
+    </Drawer>
+  )
+}
+
+export default Sidebar;
--- src/pages/SafeDashboard.js
+++ src/pages/SafeDashboard.js
@@ -1,37 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, ListItem, ListItemIcon, ListItemText, ListItemButton, Button } from '@mui/material';
+import { Box, CssBaseline, Toolbar, Button } from '@mui/material';
 
 import Navbar from '../components/safe-dashboard/Navbar';
+import Sidebar from '../components/safe-dashboard/Sidebar';
 import UserWallet from '../components/safe-dashboard/UserWallet';
 import Safe from '../components/safe-dashboard/Safe';
 import Setting from '../components/safe-dashboard/Setting';
 import Recover from '../components/safe-dashboard/Recover';
-
-import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import InventoryIcon from '@mui/icons-material/Inventory';
-import SettingsIcon from '@mui/icons-material/Settings';
-import DataSaverOnIcon from '@mui/icons-material/DataSaverOn';
-
-const drawerLinks = [
-  {
-    "text": "Your Wallet",
-    "icon": <AccountBalanceWalletIcon />
-  },
-  {
-    "text": "Your Safe",
-    "icon": <InventoryIcon />
-  },
-  {
-    "text": "Setting",
-    "icon": <SettingsIcon />
-  },
-  {
-    "text": "Recover",
-    "icon": <DataSaverOnIcon />
-  }
-];
-const drawerWidth = 200;
-
 
 function SafeDashboard({ rsContract, domainData, ethAddress, userSigner }) {
   const [safeAddress, setSafeAddress] = useState("");
@@ -68,51 +43,13 @@ function SafeDashboard({ rsContract, domainData, ethAddress, userSigner }) {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
+
       <Navbar domainData={domainData} />
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {drawerLinks.map((d, index) => (
-            <ListItem key={index} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {d.icon}
-                </ListItemIcon>
-                <ListItemText primary={d.text} onClick={() => setCurrentSection(d.text)}/>
-              </ListItemButton>
-            </ListItem>
-          ))}
-          <Divider />
-          <ListItem>
-            <div>
-              <ListItemText primary="ETH Address" />
-              <a href={`https://zksync2-testnet.zkscan.io/address/${ethAddress}`} target="_blank" rel="noopener noreferrer">
-                {ethAddress && ethAddress.substring(0,8) + "..." + ethAddress.substring(34,42)}
-              </a>
-            </div>
-          </ListItem>
-          <ListItem>
-            <div>
-              <ListItemText primary="Safe Address" />
-              <a href={`https://zksync2-testnet.zkscan.io/address/${safeAddress}`} target="_blank" rel="noopener noreferrer">
-                {safeAddress && safeAddress.substring(0,8) + "..." + safeAddress.substring(34,42)}
-              </a>
-            </div>
-          </ListItem>
-        </List>
-      </Drawer>
+      <Sidebar
+        ethAddress={ethAddress}
+        safeAddress={safeAddress}
+        setCurrentSection={setCurrentSection} />
+
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
